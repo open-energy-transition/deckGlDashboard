c77e2cf445e0f3537f9cdfee5ec89a63cabc5b24
drifter089 <akshatmittal.0007@gmail.com>
2024-09-26 15:53:02 +0700

    dropdown fixes adding controls

---
+++ components/SideBySide/MainControls.tsx
@@ -0,0 +1,108 @@
+import * as React from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export function MainControls() {
+  return (
+    <>
+      <Card className="flex flex-col justify-center align-middle items-center gap-2 p-4">
+        <Select>
+          <SelectTrigger className="w-3/5">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Fruits</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select>
+          <SelectTrigger className="w-2/5">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Fruits</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Select>
+          <SelectTrigger className="w-2/5">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Fruits</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <div className="flex flex-wrap gap-5 pt-4">
+          <div className="flex flex-col justify-center align-middle items-center">
+            <Avatar>
+              <div className="bg-slate-200 w-full h-full"></div>
+            </Avatar>
+            <div>one</div>
+          </div>
+          <div className="flex flex-col justify-center align-middle items-center">
+            <Avatar>
+              <div className="bg-slate-400 w-full h-full"></div>
+            </Avatar>
+            <div>two</div>
+          </div>
+          <div className="flex flex-col justify-center align-middle items-center">
+            <Avatar>
+              <div className="bg-slate-600 w-full h-full"></div>
+            </Avatar>
+            <div>three</div>
+          </div>
+          <div className="flex flex-col justify-center align-middle items-center">
+            <Avatar>
+              <div className="bg-slate-800 w-full h-full"></div>
+            </Avatar>
+            <div>four</div>
+          </div>
+          <div className="flex flex-col justify-center align-middle items-center">
+            <Avatar>
+              <div className="bg-black w-full h-full"></div>
+            </Avatar>
+            <div>five</div>
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+}
--- components/SolarSection/MyDropdown.tsx
+++ components/SolarSection/MyDropdown.tsx
@@ -3,22 +3,28 @@ import * as React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PieDonut from "../Charts/PieDonut";
 import BarChartSimple from "../Charts/BarChartSimple";
+import { ChartRadial } from "../Charts/ChartRadial";
+import { MainControls } from "../SideBySide/MainControls";
 
 export default function MyDropdown(props) {
   return (
-    <Tabs defaultValue="account" className={props.className}>
-      <TabsList className=" bg-gray-500/80">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-        <TabsTrigger value="chart-3">chart3</TabsTrigger>
+    <Tabs defaultValue="control" className={props.className}>
+      <TabsList>
+        <TabsTrigger value="control">CONTROLS</TabsTrigger>
+        <TabsTrigger value="chart1">chart1</TabsTrigger>
+        <TabsTrigger value="chart2">chart2</TabsTrigger>
+        <TabsTrigger value="chart3">chart3</TabsTrigger>
       </TabsList>
-      <TabsContent value="account">
-        <BarChartSimple />
+      <TabsContent value="control">
+        <MainControls />
       </TabsContent>
-      <TabsContent value="password">
-        <BarChartSimple />
+      <TabsContent value="chart1">
+        <PieDonut />
       </TabsContent>
-      <TabsContent value="chart-3">
+      <TabsContent value="chart2">
+        <ChartRadial />
+      </TabsContent>
+      <TabsContent value="chart3">
         <PieDonut />
       </TabsContent>
     </Tabs>
