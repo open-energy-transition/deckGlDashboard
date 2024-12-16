d4f6e7db004ec8239ae1a5d81661e163402138b6
Akshat Mittal <akshatmittal.0007@gmail.com>
2024-12-17 05:27:20 +0700

    adding globe section

---
+++ app/landingglobe/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useState, useEffect, use } from "react";
+import EarthNightUrl from "../../public/earth-night-light.jpg";
+import NightSkyUrl from "../../public/night-sky-light.png";
+import countries from "../../public/countries.json";
+import Globe from "react-globe.gl";
+import {
+  getGeoJsonData,
+  COUNTRY_COORDINATES,
+} from "../network/components/Links";
+import useSWR from "swr";
+
+interface FetcherResponse {
+  [key: string]: any; // Generic response type since we don't know the exact structure
+}
+
+const fetcher = (url: string): Promise<FetcherResponse> =>
+  fetch(url).then((res) => res.json());
+
+const Page = () => {
+  const [arcs, setArcs] = useState([]);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [hoverD, setHoverD] = useState();
+
+  const endpoints = Object.keys(COUNTRY_COORDINATES).map(
+    (country) => getGeoJsonData(country).countryView
+  );
+
+  const { data, error, isLoading } = useSWR(endpoints, async (urls) => {
+    const responses = await Promise.all(urls.map((url) => fetcher(url)));
+    return responses.reduce((acc, curr) => {
+      if (curr.features) {
+        acc.features = [...(acc.features || []), ...curr.features];
+      }
+      return acc;
+    }, {});
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Globe
+      width={dimensions.width}
+      height={dimensions.height}
+      globeImageUrl={EarthNightUrl.src}
+      backgroundImageUrl={NightSkyUrl.src}
+      polygonsData={data?.features}
+      polygonCapColor={() => `rgb(227, 26, 28)`}
+      polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+      //   polygonStrokeColor={() => "#111"}
+    />
+  );
+};
+
+export default Page;
