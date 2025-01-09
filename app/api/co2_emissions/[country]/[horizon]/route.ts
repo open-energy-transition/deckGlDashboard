bc5212bc147a771d48f09985d2a0c6a500700fef
Bryan Ramírez <42655811+BryanFran@users.noreply.github.com>
2025-01-09 13:45:21 -0500

    Main page (#70)
    This PR improves the visual representation of the globe by:
    
    - Adding distinct colors for each country based on their national flags
    - Implementing hover labels that display country names
    - Adding subtle transparency effects for polygon sides
    
    This PR also adds the APIs for handling charts to the main page.

---
+++ app/api/co2_emissions/[country]/[horizon]/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: Number(process.env.POSTGRES_PORT),
+});
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { country: string; horizon: string } }
+) {
+  const { country, horizon } = params;
+
+  try {
+    const result = await pool.query(
+      `
+        SELECT *
+        FROM public.co2_emissions
+        WHERE country_code = $1 AND horizon = $2;
+      `,
+      [country, horizon]
+    );
+
+    return NextResponse.json({ data: result.rows });
+  } catch (error) {
+    console.error('Error fetching CO2 emissions data:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch CO2 emissions data' },
+      { status: 500 }
+    );
+  }
+} 
