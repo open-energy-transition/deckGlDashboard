2a8866e6ad242afd98e9cd3d5a789b6780d2b102
BryanFran <brramirez@unal.edu.co>
2025-02-19 10:59:22 -0500

    fix: simplify GlobeOnly component and fix ref type

--- app/landingglobe/JustGlobe.tsx
+++ app/landingglobe/JustGlobe.tsx
@@ -1,26 +1,11 @@
 "use client";
 
-import React, { forwardRef, useEffect, useRef, MutableRefObject } from "react";
+import React, { forwardRef } from "react";
 import R3fGlobe, { GlobeMethods, GlobeProps } from "r3f-globe";
 
-const GlobeOnly = forwardRef<GlobeMethods, GlobeProps>((props, ref) => {
-  const globeRef = useRef<GlobeMethods | undefined>(undefined);
-
-  useEffect(() => {
-    if (globeRef.current) {
-
-      if (typeof ref === 'function') {
-        ref(globeRef.current);
-      } else if (ref) {
-        ref.current = globeRef.current;
-      }
-    }
-  }, [ref]);
-
-  return (
-    <R3fGlobe {...props} ref={globeRef as MutableRefObject<GlobeMethods | undefined>} />
-  );
-});
+const GlobeOnly = forwardRef<GlobeMethods, GlobeProps>((props, ref) => (
+  <R3fGlobe {...props} ref={ref as React.MutableRefObject<GlobeMethods | undefined>} />
+));
 
 GlobeOnly.displayName = "GlobeOnly";
 
