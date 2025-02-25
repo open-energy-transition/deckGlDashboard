626c67660943a548ca74485cbb92514e8dbde50a
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-02-26 02:10:50 +0700

    making minimal version

--- app/landingglobe/popups/ElectricityPriceComponent.tsx
+++ app/landingglobe/popups/ElectricityPriceComponent.tsx
@@ -48,6 +48,8 @@ const ElectricityPriceComponent = ({
 
   const [mouse, ref] = useMouse();
 
+  const textRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (!contentRef.current) return;
 
@@ -55,6 +57,11 @@ const ElectricityPriceComponent = ({
     gsap.to(contentRef.current, {
       delay: 0.08,
       x: x,
+      y: y * 0.5,
+    });
+    gsap.to(textRef.current, {
+      delay: 0.08,
+      x: x + 12 * 34,
       y: y * 0.5,
     });
   }, [mouse]);
@@ -162,49 +169,36 @@ const ElectricityPriceComponent = ({
   }, [fetchData]);
 
   return (
-    <Card
-      className={`fixed top-0 left-0  h-0 w-0 z-40  mx-auto text-accent-foreground bg-background text-center opacity-1 overflow-hidden border-border border-2 m-2`}
-      ref={contentRef}
-    >
-      {!loading && (
-        <div className="w-full h-full grid grid-cols-11">
-          <CircleFlag
-            countryCode={hoveredCountry.toLowerCase()}
-            height={50}
-            className="pt-2 w-full aspect-square col-span-11 h-20 mx-auto md:h-auto md:col-span-2 md:ml-2 md:mt-2"
-          />
-          <div className="col-span-11 md:col-span-9 flex flex-col justify-center">
-            <p className="text-xs text-muted-foreground">Investment Required</p>
-            <p className="text-3xl font-bold mb-1">
-              €{data.totalInvestmentNeeded.toFixed(2)}{" "}
-              <span className="hidden md:inline">Billion</span>
-              <span className="md:hidden">B</span>
-            </p>
-
-            <div className="flex justify-around">
-              <div>
-                <p className="text-sm">2021</p>
-                <p>
-                  <span className="font-semibold">{data.electricityPrice2021.toFixed(2)}</span>
-                  <span className="block md:inline font-normal"> €/MWh</span>
-                </p>
-              </div>
-              <div>
-                <p className="text-sm">2050</p>
-                <p>
-                  <span className="font-semibold">{data.electricityPrice2050.toFixed(2)}</span>
-                  <span className="block md:inline font-normal"> €/MWh</span>
-                </p>
-              </div>
+    <>
+      <Card
+        className={`fixed top-0 left-0  h-0 w-0 z-40  mx-auto text-accent-foreground bg-background text-center opacity-1 overflow-hidden border-border border-2 m-2`}
+        ref={contentRef}
+      >
+        {!loading && (
+          <div className="w-full h-full grid grid-cols-11">
+            <CircleFlag
+              countryCode={hoveredCountry.toLowerCase()}
+              height={50}
+              className="pt-2 w-full aspect-square col-span-11 h-28 mx-auto md:h-auto md:col-span-2 translate-x-3 translate-y-3"
+            />
+            <div className="col-span-11 md:col-span-9 flex flex-col justify-center">
+              <p className="text-muted-foreground w-full  h-[10%] flex justify-center items-center">
+                Investment Required
+              </p>
+              <p className="text-3xl font-bold mb-1">
+                €{data.totalInvestmentNeeded.toFixed(2)}{" "}
+                <span className="hidden md:inline">Billion</span>
+                <span className="md:hidden">B</span>
+              </p>
+            </div>
+            <div className="hidden md:block md:col-span-11 -mt-12">
+              <GenerationMixglobe data={data.generationMix} />
             </div>
           </div>
-          <div className="hidden md:block md:col-span-11 -mt-12">
-            <GenerationMixglobe data={data.generationMix} />
-          </div>
-        </div>
-      )}
-      {loading && <p>Loading...</p>}
-    </Card>
+        )}
+        {loading && <p>Loading...</p>}
+      </Card>
+    </>
   );
 };
 
