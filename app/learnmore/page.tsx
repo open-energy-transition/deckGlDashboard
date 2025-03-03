2b5cf8060c20ef1682244af7dac9d496e3bcc87b
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-03-04 00:47:35 +0700

    rearrange sections

--- app/learnmore/page.tsx
+++ app/learnmore/page.tsx
@@ -1,3 +1,5 @@
+import { GithubSection } from "@/components/Homepage/github";
+import { IntroOETSection } from "@/components/Homepage/introOET";
 import { ConceptsSection } from "@/components/Learnmore/Concepts";
 import { ExplainGlobeSection } from "@/components/Learnmore/ExplainGlobe";
 import { ExplainNetworkSection } from "@/components/Learnmore/ExplainNetwork";
@@ -5,9 +7,8 @@ import { ExplainNetworkSection } from "@/components/Learnmore/ExplainNetwork";
 export default function Page() {
   return (
     <div className="w-screen min-h-screen overflow-y-scroll overflow-x-hidden bg-background flex flex-col items-center justify-center gap-4 p-4 lg:gap-8 lg:p-0 box-border max-w-screen">
-      <ExplainGlobeSection />
-      <ExplainNetworkSection />
-      <ConceptsSection />
+      <IntroOETSection />
+      <GithubSection />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,17 @@ import { CreditsSection } from "@/components/Homepage/credits";
 import { CommunitySection } from "@/components/Homepage/community";
 import ScrollButton from "@/components/Homepage/ScrollButton";
 import { IntroOETSection } from "@/components/Homepage/introOET";
+import { ExplainGlobeSection } from "@/components/Learnmore/ExplainGlobe";
+import { ExplainNetworkSection } from "@/components/Learnmore/ExplainNetwork";
+import { ConceptsSection } from "@/components/Learnmore/Concepts";
 
 export default function Page() {
   return (
     <div className="w-screen min-h-screen overflow-y-scroll overflow-x-hidden bg-background flex flex-col items-center justify-center gap-4 p-4 lg:gap-8 lg:p-0 box-border max-w-screen">
       <HeroSection />
-      <ScenariosSection />
-      <IntroOETSection />
-      <GithubSection />
-      <CreditsSection />
+      <ExplainGlobeSection />
+      <ExplainNetworkSection />
+      <ConceptsSection />
       <CommunitySection />
     </div>
   );
