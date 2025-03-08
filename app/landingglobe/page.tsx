1bfe641aa58d178569345cdf3b3aa077f4a83d75
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-03-08 21:23:37 +0700

    update text and add map legend

--- app/landingglobe/page.tsx
+++ app/landingglobe/page.tsx
@@ -21,7 +21,19 @@ export default function LandingGlobe() {
     <div className="relative w-full h-screen">
       <Globe />
       <MainPageNav open={isOpen} setIsOpen={setIsOpen} />
-      {/* <ElectricityPriceComponent /> */}
+      <div className="absolute border-border border bg-background top-4 right-2 hidden md:block w-34 h-24 p-2 rounded-lg">
+        Hover a country and see the investment needed in
+        <ul className="list-none">
+          <li>
+            <span className="inline-block w-3 h-3 bg-[hsl(45,100%,50%)] mx-1 rounded-sm" />{" "}
+            Solar
+          </li>
+          <li>
+            <span className="inline-block w-3 h-3 bg-[hsl(195,85%,65%)] mx-1 rounded-sm" />{" "}
+            Wind
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
