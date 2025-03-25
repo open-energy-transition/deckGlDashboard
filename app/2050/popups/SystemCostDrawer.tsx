06e714febc95eb87062effe36200fa98c38402e4
ElectricMountains <193539781+ElectricMountains@users.noreply.github.com>
2025-03-25 21:27:36 +0000

    Remove - from electricity price

--- app/2050/popups/SystemCostDrawer.tsx
+++ app/2050/popups/SystemCostDrawer.tsx
@@ -160,7 +160,8 @@ const SystemCostDrawer = ({
                   tooltipInfo={ElectricityPrice_info}
                   className="h-5 w-5"
                 />{" "}
-                - {electricityPriceState2021[0]?.electricity_price ?? "N/A"}{" "}€/MWh
+                - {electricityPriceState2021[0]?.electricity_price ?? "N/A"}{" "}
+                €/MWh
               </p>
             ) : (
               <p className="w-full text-2xl font-semibold text-card-foreground text-center">
@@ -186,7 +187,7 @@ const SystemCostDrawer = ({
                   tooltipInfo={ElectricityPrice_info}
                   className="h-5 w-5"
                 />{" "}
-                - {electricityPriceState2050[0]?.electricity_price ?? "N/A"}{" "}€/MWh
+                {electricityPriceState2050[0]?.electricity_price ?? "N/A"} €/MWh
               </p>
             ) : (
               <p className="w-full text-2xl font-semibold text-card-foreground text-center">
