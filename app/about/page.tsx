bc029945416bbd99d71e46ddf7b3087c19fe096d
eliasheininger <eliasheininger@web.de>
2025-04-02 13:12:46 +0700

    added hero section for the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,3 +3,4 @@ import { AboutHero } from "@/components/OverveiwComponents/aboutHero";
 export default function Page() {
   return <AboutHero />;
 }
+
--- components/OverveiwComponents/aboutHero.tsx
+++ components/OverveiwComponents/aboutHero.tsx
@@ -1,17 +1,28 @@
+import Image from "next/image";
+
 export const AboutHero = () => {
   return (
-    <section className="container w-full">
-      <div className="">
-        <div className="font-sans flex flex-row text-card-foreground  text-3xl md:text-4xl lg:text-6xl font-bold">
-          <h1 className="flex flex-col">
-            {" "}
-            Methods&
-            <span className=" dark:text-accent text-destructive ">
-              Limitations
-            </span>
-          </h1>
-        </div>
-      </div>
+    <section className="container relative w-full h-[27rem] overflow-hidden">
+     
+            <div className="relative z-10 flex flex-col justify-center h-full font-sans text-card-foreground text-3xl md:text-4xl lg:text-6xl font-bold">
+              <h1 className="flex flex-col">
+                {" "}
+               Methods&
+                <span className=" dark:text-accent text-destructive">
+                 Limitations
+                </span>
+             
+              </h1>
+             
+          </div>
+             <Image
+              src="/images/credits/windturbine.png"
+              alt="Background"
+              fill
+              objectFit="cover"
+              className="z-0 rounded-xl"
+      />
+     
     </section>
   );
 };
