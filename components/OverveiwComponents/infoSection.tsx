bb20277338abceea5ca5a5ee10f67f07241b72dd
Max Parzen <max.parzen@ed.ac.uk>
2025-04-21 18:36:15 +0200

    fix overflow

--- components/OverveiwComponents/infoSection.tsx
+++ components/OverveiwComponents/infoSection.tsx
@@ -1,7 +1,7 @@
 export const InfoSection = () => {
   return (
     <section className="container w-full pt-24">
-      <div className="flex flex-row flex-wrap sm:flex-nowrap gap-[8rem] items-start border-t-[0.5px] border-foreground pt-4 pb-16  justify-between ">
+      <div className="flex flex-row flex-wrap gap-[4rem] items-start border-t-[0.5px] border-foreground pt-4 pb-16  justify-between ">
         <div className=" font-bold font-sans  max-w-[22rem] text-5xl md:text-6xl lg:text-6xl dark:text-secondary">
           {" "}
           Why did we build
@@ -14,7 +14,7 @@ export const InfoSection = () => {
           help accelerate the global energy transition. While this platform is
           still a prototype, our vision is to enable answers to key questions
           such as:
-          <ul className="mt-2 list-disc pl-5 space-y-2 md:text-[1.25rem] text-[1rem] text-foreground lg:w-[50rem]">
+          <ul className="mt-2 list-disc pl-5 space-y-2">
             <li>
               How much investment is needed to achieve a 100% clean or net-zero
               power system?
