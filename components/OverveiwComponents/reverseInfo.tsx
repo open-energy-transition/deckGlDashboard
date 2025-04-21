1f411e066b78b34c54458f25054b082b0d8632d9
Max Parzen <maximilian.parzen@gmail.com>
2025-04-21 16:11:12 +0200

    Adjust content and change broken links (#188)
    Adjusting the content:
    - rewrite landing page
    - rewrite about page
    - fix broken discord link
    
    ---------
    
    Co-authored-by: Ekaterina <e.v.kasilova@gmail.com>

--- components/OverveiwComponents/reverseInfo.tsx
+++ components/OverveiwComponents/reverseInfo.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const ReverseInfo = () => {
   return (
     <section className="container w-full pt-24">
@@ -10,9 +12,31 @@ export const ReverseInfo = () => {
           </span>
         </div>
         <div className=" md:text-[1.25rem] text-[1rem] max-w-[45rem] text-foreground">
-          Open Energy Transition (OET) researches and develops open-source tools
-          and methodologies to drive the global energy transition towards 100%
-          renewable energy.
+          Open Energy Transition (OET) is a non-profit organization accelerating
+          the global shift to clean energy by making cutting-edge tools and
+          insights accessible to energy planners —helping steer billions in
+          investment based on trusted information.
+          <br />
+          <br />
+          We advance open-source software and open data as powerful enablers of
+          accessibility, transparency, and collaboration—helping stakeholders
+          plan faster, lower system costs, and strengthen grid resilience. We
+          also provide training and best-practice guidance to ensure planners
+          and policymakers can confidently use these tools to drive effective,
+          data-informed decisions.
+          <br />
+          <br />
+          Want to build similar applications? Want to tailor this app for your
+          need? Explore support opportunities by emailing
+          <strong> info@openenergytransition.org</strong>. More information
+          about OET can be found at{" "}
+          <Link
+            href="https://openenergytransition.org/"
+            target="_blank"
+            className="dark:text-secondary text-destructive"
+          >
+            https://openenergytransition.org.
+          </Link>{" "}
         </div>
       </div>
     </section>
