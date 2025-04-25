799f668cc58167bc0c409d24ce7de9ef40e7f2ba
ekatef <e.v.kasilova@gmail.com>
2025-04-25 13:05:38 +0200

    Add a mention of the capacity expansion

--- utilities/TooltipInfo/ExplainerText/InstalledCapacity.ts
+++ utilities/TooltipInfo/ExplainerText/InstalledCapacity.ts
@@ -26,5 +26,5 @@ export const Installed_capacity_info: Installed_capacity_info_type = {
     "Brownfield capacity for 2021 is extracted from available open databases, while the installed capacity values for 2050 are derived from the optimization run.",
   values: "Installed capacity represents the generation units available in the power system to meet electricity demand.",
   comparison:
-    "Changes in installed capacity between now and 2050, along with the capacity expansions needed to meet future demand and achieve the net-zero target in a cost-optimal manner.",
+    "Required Capacity Expansion refers to the adjustments in installed generation capacity necessary to meet future demand and achieve the net-zero target in a cost-effective manner.",
 };
